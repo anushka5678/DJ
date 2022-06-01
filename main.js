5b734a2f3bb0b8f3f59dcab6f5fa1e45726e5748
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2022-06-01 16:23:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX = 0;
+leftWristY=0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -8,12 +12,34 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+    if (results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY=" + leftWristY);
+
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("rightWristX = " + rightWristX + "rightWristY=" + rightWristY);
+    }
+}
+
 function draw(){
     image(video, 0, 0, 600, 500);
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 function stop(){
     song.stop();
